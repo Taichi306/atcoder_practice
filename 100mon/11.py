341a7b145b596e57e07490e68ec1017d4469d61e
Taichi306 <saitaichi0423@gmail.com>
2022-06-16 19:53:22 +0900

    aaa

---
+++ 100mon/11.py
@@ -0,0 +1,12 @@
+# N, M = map(int, input().split())
+# switches = []
+# for _ in range(M):
+#     temp = list(map(int, input().split()))
+#     switches.append(temp)
+# p = list(map(int, input().split()))
+
+"""
+switchのon/offを1/0としてbit探索して, pと整合性が取れかつ
+電球が全て点灯してればいい?
+"""
+
--- ABC_cpp/a.cpp
+++ ABC_cpp/a.cpp
@@ -1,4 +1,21 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
-    printf("hello\n");
+    int N;
+    cin >> N;
+    vector<int> score;
+    for (int i = 0; i < N; i++) {
+        cin >> score[i];
+    }
+    // int avg;
+    // for (int i = 0; i < N; i++) {
+    //     avg += score[i];
+    // }
+    // avg /= N;
+    cout << score[1] << endl;
+    // cout << avg << endl;
+    // for (int i = 0; i < score.size(); i++) {
+    //     cout << abs(avg - score.at(i)) << endl;
+    // }
 }
